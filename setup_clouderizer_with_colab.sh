a701682223999e75d97499aa795c947322059d72
ronaldokun <ronaldokun@gmail.com>
2018-11-28 21:33:26 -0200

    Added pytorch install from pytorch docs
    The previous script wasn't installing the pytorch version 1.x

--- setup_clouderizer_with_colab.sh
+++ setup_clouderizer_with_colab.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
-if [ ! -d course-v3 ]; then
-        pip3 install pillow==4.1.1 --upgrade
-        pip3 install torch_nightly -f https://download.pytorch.org/whl/nightly/cu92/torch_nightly.html --upgrade
-
-        sed -n -e '/^tmpfs \/dev\/shm tmpfs defaults,size=/!p' -e '$atmpfs \/dev\/shm tmpfs defaults,size=1g 0 0' -i /etc/fstab
-        mount -o remount /dev/shm
-
-        mkdir -p /content/.torch/
-        mkdir -p /content/.fastai/
-        ln -s /content/clouderizer/fastai-v3/data/ /content/.fastai/data 
-        rm -rf /content/sample_data/
-        git clone https://github.com/fastai/course-v3
-        pip3 install fastai --upgrade
-        cd course-v3
-        git pull
-fi
+pip3 install pillow==4.1.1 --upgrade
+pip3 install numpy torchvision_nightly --upgrade
+pip3 install torch_nightly -f https://download.pytorch.org/whl/nightly/cu92/torch_nightly.html --upgrade
+mkdir -p /content/.torch/
+mkdir -p /content/.fastai/
+ln -s /content/clouderizer/fastai-v3/data/ /content/.fastai/data 
+pip3 install fastai --upgrade
 
