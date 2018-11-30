742335a7e217610165fcd982cc9f876ba575cc23
ronaldokun <ronaldokun@gmail.com>
2018-11-29 22:27:08 -0200

    Trying to fix bug installing torch 0.4.1
    Instead of installing torch 1.0

--- setup_clouderizer_with_colab.sh
+++ setup_clouderizer_with_colab.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 pip3 install pillow==4.1.1 --upgrade
+pip3 uninstall numpy torch==0.4.1 -y
 pip3 install numpy torchvision_nightly --upgrade
 pip3 install torch_nightly -f https://download.pytorch.org/whl/nightly/cu92/torch_nightly.html --upgrade
 mkdir -p /content/.torch/
 mkdir -p /content/.fastai/
 ln -s /content/clouderizer/fastai-v3/data/ /content/.fastai/data 
 pip3 install fastai --upgrade
+git clone https://github.com/fastai/course-v3.git
+mv course-v3 /content/code/
 
