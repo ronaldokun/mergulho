7a541a25923af64ec04fb3d62196a243fed29a6c
ronaldokun <ronaldokun@gmail.com>
2018-12-01 12:58:57 -0200

    Fixed installation issue
    Directly installing torchvision was conflicting with the torch_nightly installation. Just installing torch_nightly then fastai worked.

--- setup_clouderizer_with_colab.sh
+++ setup_clouderizer_with_colab.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-pip3 install pillow==4.1.1 --upgrade
-pip3 uninstall numpy torch==0.4.1 -y
-pip3 install numpy torchvision_nightly --upgrade
 pip3 install torch_nightly -f https://download.pytorch.org/whl/nightly/cu92/torch_nightly.html --upgrade
 mkdir -p /content/.torch/
 mkdir -p /content/.fastai/
 ln -s /content/clouderizer/fastai-v3/data/ /content/.fastai/data 
 pip3 install fastai --upgrade
 git clone https://github.com/fastai/course-v3.git
-mv course-v3 /content/code/
+mv course-v3 /content/fastai-v3/code/
 
