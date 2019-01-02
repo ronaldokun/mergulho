3c1f7e7d32ba95d79d6f41c42526e7b95661c0fa
ronaldokun <ronaldokun@gmail.com>
2019-01-02 19:00:55 -0200

    Simples setup since the release of pytorch 1.0

--- setup_clouderizer_with_colab.sh
+++ setup_clouderizer_with_colab.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 pip3 install --upgrade pip
 pip3 install dataclasses
-pip3 uninstall numpy torchvision_nightly torch torch_nightly -y
-pip3 install torch_nightly -f https://download.pytorch.org/whl/nightly/cu92/torch_nightly.html --upgrade
+pip3 install fastai --upgrade
 mkdir -p /content/.torch/
 mkdir -p /content/.fastai/
 echo data_path: /content/clouderizer/fastai-v1/data > ~/.fastai/config.yml
 echo model_path: /content/clouderizer/fastai-v1/out >> ~/.fastai/config.yml
-pip3 install fastai --upgrade
 
