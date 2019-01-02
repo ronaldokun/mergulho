7c3be8c9f7407bf990de781ac3bf7c111dcf2fa2
ronaldokun <ronaldokun@gmail.com>
2019-01-02 20:19:54 -0200

    Excluded config.yml definition

--- setup_clouderizer_with_colab.sh
+++ setup_clouderizer_with_colab.sh
@@ -4,6 +4,4 @@ pip3 install dataclasses
 pip3 install fastai --upgrade
 mkdir -p /content/.torch/
 mkdir -p /content/.fastai/
-echo data_path: /content/clouderizer/fastai-v1/data > ~/.fastai/config.yml
-echo model_path: /content/clouderizer/fastai-v1/out >> ~/.fastai/config.yml
 
