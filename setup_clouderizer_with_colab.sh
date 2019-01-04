6b5ea4893875fad537ede3e1db18f3dcc3adc687
ronaldokun <ronaldokun@gmail.com>
2019-01-04 00:00:49 -0200

    Abandoned old jupyter extensions

--- setup_clouderizer_with_colab.sh
+++ setup_clouderizer_with_colab.sh
@@ -2,10 +2,6 @@
 pip3 install --upgrade pip
 pip3 install dataclasses
 pip3 install fastai --upgrade
-pip3 install https://github.com/ipython-contrib/jupyter_contrib_nbextensions/tarball/master
-pip3 jupyter contrib nbextension install --user
-jupyter nbextension enable collapsible_headings/main
-pip3 install jupyterlab-discovery
 mkdir -p /content/.torch/
 mkdir -p /content/.fastai/
 mkdir -p /content/clouderizer/fastai-v3/out/models
