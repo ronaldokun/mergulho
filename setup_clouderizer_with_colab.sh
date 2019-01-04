df3ea7bc0e2c499c967d196ae3e1621571661b98
ronaldokun <ronaldokun@gmail.com>
2019-01-04 00:15:13 -0200

    Removing jupyter-discovery extension
    It seems the install of jupyter-discovery os breaking the jupyterlab install. Remove it.

--- setup_clouderizer_with_colab.sh
+++ setup_clouderizer_with_colab.sh
@@ -2,7 +2,6 @@
 pip3 install --upgrade pip
 pip3 install dataclasses
 pip3 install fastai --upgrade
-pip3 install jupyterlab-discovery
 mkdir -p /content/.torch/
 mkdir -p /content/.fastai/
 mkdir -p /content/clouderizer/fastai-v3/out/models
