e1d86d22adebc212fac75cf8192b2a1ec84d2624
ronaldokun <ronaldokun@gmail.com>
2019-01-06 17:42:42 -0200

    Fixed creation of config.yml file.

--- setup_clouderizer_with_colab.sh
+++ setup_clouderizer_with_colab.sh
@@ -5,8 +5,8 @@ pip3 install fastai --upgrade
 mkdir -p /content/.torch/
 mkdir -p /content/.fastai/
 mkdir -p /content/clouderizer/fastai-v3/out/models
-cat data_path: /content/clouderizer/fastai-v3/data/ > /content/.fastai/config.yml
-cat model_path: /content/clouderizer/fastai-v3/out/ >> /content/.fastai/config.yml
+echo data_path: /content/clouderizer/fastai-v3/data/ > /content/.fastai/config.yml
+echo model_path: /content/clouderizer/fastai-v3/out/ >> /content/.fastai/config.yml
 
 
 
