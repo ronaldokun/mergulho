ea3d193ac23faae29a4a2440ff562cd63be35521
ronaldokun <ronaldokun@gmail.com>
2019-01-16 12:16:38 -0200

    Updating the fastai install

--- setup_clouderizer_with_colab.sh
+++ setup_clouderizer_with_colab.sh
@@ -2,11 +2,18 @@
 pip3 install --upgrade pip
 pip3 install dataclasses
 pip3 install fastai --upgrade
+pip3 install jupyter --upgrade 
+pip3 install jupyter_nbextensions_configurator --upgrade 
+pip3 install jupyter_contrib_nbextensions --upgrade
+jupyter contrib nbextension install
+jupyter nbextensions_configurator enable
+pip3 install --no-deps pretrainedmodels
+
 mkdir -p /content/.torch/
 mkdir -p /content/.fastai/
-mkdir -p /content/clouderizer/fastai-v3/out/models
-echo data_path: /content/clouderizer/fastai-v3/data/ > /content/.fastai/config.yml
-echo model_path: /content/clouderizer/fastai-v3/out/ >> /content/.fastai/config.yml
+mkdir -p /content/clouderizer/fastai-1.0/out/models
+echo data_path: /content/clouderizer/fastai-1.0/data/ > /content/.fastai/config.yml
+echo model_path: /content/clouderizer/fastai-1.0/out/ >> /content/.fastai/config.yml
 
 
 
